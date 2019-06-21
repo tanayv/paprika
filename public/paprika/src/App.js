1a2780aeae52839c81ad38b5083b9485d31592d4
Tanay Vardhan <tvardha2@illinois.edu>
2019-06-22 01:16:28 +0530

    fix typo in API

---
+++ public/paprika/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Axios from 'axios';
+
+function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+  if (typeof code !== undefined) {
+    console.log("RECEIVED CODE", code);
+    Axios.post("https://berlinwall.herokuapp.com/api/auth?code="+code);
+  }
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://github.com/login/oauth/authorize?client_id=Iv1.ab929d25982c75d7"
+        >
+          Sign In Using GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
