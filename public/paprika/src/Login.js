9076917b47fb5032da576ec06c8c99a1362268de
Tanay Vardhan <tvardha2@illinois.edu>
2019-06-22 03:27:02 +0530

    client_secret for backend and scope for frontend

--- public/paprika/src/Login.js
+++ public/paprika/src/Login.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Login = (props) => {
-    
+
     return (
         <div className="login-wrapper">
             <div className="login-box">
                 <h1 class="serif">Berlin Wall</h1>
-                <a className="login-link" href="https://github.com/login/oauth/authorize?client_id=Iv1.ab929d25982c75d7"
+                <a className="login-link" href="https://github.com/login/oauth/authorize?client_id=Iv1.ab929d25982c75d7&scope=repo"
                 ><button>Continue with GitHub</button></a>
             </div>
         </div>
