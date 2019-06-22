72b47165f8ac0446e4eb5ac56f0648284a48d32b
Tanay Vardhan <tvardha2@illinois.edu>
2019-06-22 17:44:35 +0530

    Updates

---
+++ precache-manifest.ebc4439479ad12f71973bcd114782f46.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9c11f2793d5738b6e6b9be793c79b8",
+    "url": "/paprika/index.html"
+  },
+  {
+    "revision": "ed2f08768f9763041c34",
+    "url": "/paprika/static/css/main.ab80326e.chunk.css"
+  },
+  {
+    "revision": "af478213e1578da99d8d",
+    "url": "/paprika/static/js/2.6225c6a3.chunk.js"
+  },
+  {
+    "revision": "ed2f08768f9763041c34",
+    "url": "/paprika/static/js/main.7c017def.chunk.js"
+  },
+  {
+    "revision": "a1490e8ccf55b6774ca2",
+    "url": "/paprika/static/js/runtime~main.1a284417.js"
+  },
+  {
+    "revision": "1de1af239aa86a0ef7eef6a9538b0f6a",
+    "url": "/paprika/static/media/bhogra.1de1af23.png"
+  },
+  {
+    "revision": "50b51b1cd6fac14ff91d1a51a62ce96f",
+    "url": "/paprika/static/media/bishk.50b51b1c.png"
+  },
+  {
+    "revision": "f8b7b4e45ff74361d60ea260fde13941",
+    "url": "/paprika/static/media/bk.f8b7b4e4.png"
+  },
+  {
+    "revision": "7652bf606daf12993f0dd09f3fd3b35b",
+    "url": "/paprika/static/media/kshe.7652bf60.png"
+  },
+  {
+    "revision": "c7b4b3ffbc01983633d656e900fc81b9",
+    "url": "/paprika/static/media/logo.c7b4b3ff.png"
+  },
+  {
+    "revision": "0e7c1ecc9439eeeb55762ed4db24d010",
+    "url": "/paprika/static/media/madhav.0e7c1ecc.png"
+  },
+  {
+    "revision": "bca7fa9df2ee7a9f97bdcb4a05ce69f2",
+    "url": "/paprika/static/media/sak.bca7fa9d.png"
+  },
+  {
+    "revision": "189a9088be7d2d2e42366e93fc20bb4a",
+    "url": "/paprika/static/media/vardy.189a9088.png"
+  }
+]);
