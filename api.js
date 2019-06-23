4a13c0ff3baa5e4a7ae927993ae46e93a9c89d0b
Tanay Vardhan <tvardha2@illinois.edu>
2019-06-23 13:55:27 +0530

    add space in token

--- api.js
+++ api.js
@@ -17,7 +17,7 @@ router.get("/", (req, res) => {
 
 router.get("/auth", (req, res) => {
     const code = req.query.code;
-    console.log(code);
+    console.log("Access token exchange request for: " + code);
 
     let config = {
         headers: {
@@ -31,20 +31,82 @@ router.get("/auth", (req, res) => {
         "code": code,
       }, config)
       .then(function (response) {
-        console.log(response.data);
+        console.log("Access token received");
         state.access_token = response.data.access_token;
         res.json({"Msg": "Access Token Saved"});
       })
       .catch(function (error) {
         res.json(error);
-        console.log(error);
+        console.log("Error with receiving access token");
       });
-
-      
 });
 
+router.get("/data", (req, res) => {
+    let data = {
+      "bishk": [],
+      "bk": [],
+      "kshe": [],
+      "bhogra": [],
+      "madhav": [],
+      "sak": [],
+      "vardy": []
+    };
+
+    /* Query each member's directory and store URL's in array */
+    let bishkPromise = axios.get(`https://api.github.com/repos/tanayv/paprika/contents/public/paprika/public/bucket/bishk`, {
+      headers: {
+        Authorization: "token " + state.access_token
+      }
+    });/*.then((response) => {
+      data.bishk = response
+    }).catch((error) => {
+      console.log("There was an error in getting data for bishk");
+    });*/
+
+    let bkPromise = axios.get(`https://api.github.com/repos/tanayv/paprika/contents/public/paprika/public/bucket/bk`, {
+      headers: {
+        Authorization: "token " + state.access_token
+      }
+    });
+
+    let kshePromise = axios.get(`https://api.github.com/repos/tanayv/paprika/contents/public/paprika/public/bucket/kshe`, {
+      headers: {
+        Authorization: "token " + state.access_token
+      }
+    });
+
+    let bhograPromise = axios.get(`https://api.github.com/repos/tanayv/paprika/contents/public/paprika/public/bucket/bk`, {
+      headers: {
+        Authorization: "token " + state.access_token
+      }
+    });
+
+    let madhavPromise = axios.get(`https://api.github.com/repos/tanayv/paprika/contents/public/paprika/public/bucket/bk`, {
+      headers: {
+        Authorization: "token " + state.access_token
+      }
+    });
+
+    let sakPromise = axios.get(`https://api.github.com/repos/tanayv/paprika/contents/public/paprika/public/bucket/bk`, {
+      headers: {
+        Authorization: "token " + state.access_token
+      }
+    });
+
+    let vardyPromise = axios.get(`https://api.github.com/repos/tanayv/paprika/contents/public/paprika/public/bucket/bk`, {
+      headers: {
+        Authorization: "token " + state.access_token
+      }
+    });
+
+    Promise.all([bishkPromise, bkPromise, kshePromise, bhograPromise, madhavPromise, sakPromise, vardyPromise]).then((values) => {
+      res.json(value);
+    });
+    
+})
+
 router.post('/upload', upload.single('example'), (req, res, next) => {
-    const encoded = Buffer.from(fs.readFileSync(req.file.path)).toString("base64").toString();
+    const encoded = Buffer.from(fs.readFileSync(req.file.path)).toString("base64");
     let payload = {
       "message": "File added using Paprika",
       "committer": {
@@ -61,12 +123,10 @@ router.post('/upload', upload.single('example'), (req, res, next) => {
     let path = `https://api.github.com/repos/tanayv/paprika/contents/public/paprika/public/bucket/test42.png?client_secret=effd0f79f2737a8b2a008b250c029860b36fc380`;
     axios.put(path, payload, config)
       .then(function (response) {
-        console.log("File Upload Response", response.data);
         res.json({"fur": response.data});
       })
       .catch(function (error) {
         res.json(error.response);
-        console.log(error.response);
       });
   })
 
